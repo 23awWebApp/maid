0f5600b498e4312da150c1fff5d3ad3ce0ff35ec
MiyaKCheng <113998121+MiyaKCheng@users.noreply.github.com>
2024-07-08 12:26:41 +0900

    index updated

--- app/index.tsx
+++ app/index.tsx
@@ -1,43 +1,20 @@
-import React from 'react';
-import { Box, VStack, HStack } from "@gluestack-ui/themed";
-import Image from "../assets/images/adaptive-icon.png";
-import { Image as RNImage } from "react-native";
-import SquareButton from "../components/SquareButton";
-import { Link } from 'expo-router';
-
+import React, { useEffect } from 'react';
+import { Link, useRouter } from 'expo-router';
 
 export default function Home() {
+  const router = useRouter();
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push('/tabs/MainPage');
+    }, 5000);
+
+    return () => clearTimeout(timer); // Cleanup the timer if the component unmounts
+  }, []);
 
   return (
-    <Box flex={1} backgroundColor="$black">
-      {/* for the character */}
-      <Box flex={1 / 3}>
-        <RNImage
-          source={Image}
-          style={{ width: "100%", height: "100%", resizeMode: "cover" }}
-        />
-      </Box>
-
-
-      {/* for buttons */}
-      <Box flex={2 / 3} justifyContent="center" alignItems="center" paddingHorizontal="$4">
-        <VStack space={"4xl"}>
-          <HStack space={"4xl"}>
-            <SquareButton href="/page1" text="Go to Page 1" />
-            <SquareButton href="/page2" text="Go to Page 2" />
-          </HStack>
-          <HStack space={"4xl"}>
-            <SquareButton href="/page3" text="Go to Page 3" />
-            <SquareButton href="/page4" text="Go to Page 4" />
-          </HStack>
-          <HStack space={"4xl"}>
-            <SquareButton href="/page5" text="Go to Page 5" />
-            <SquareButton href="/tabs/tab1" text="Go to Page 6" />
-          </HStack>
-        </VStack>
-      </Box>
-    </Box>
+    <>
+      welcome to maid app!
+    </>
   );
 }
-
